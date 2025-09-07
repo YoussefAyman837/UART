ab552cfea7c0353e3823667b88d8b245c9fd346d
YoussefAyman837 <yousefayman837@gmail.com>
2025-09-07 17:35:20 +0300

    Update UART_tb.sv

--- UART_tb.sv
+++ UART_tb.sv
@@ -6,14 +6,9 @@ module UART_tb;
     // UART Interface
 
     // Transmit Interface
-    logic [7:0] tx_data;
-    logic tx_valid;
-    logic tx_ready;
     logic baud_tick_o;
     // Receive Interface
-    logic [7:0] rx_data;
-    logic rx_valid;
-    logic rx_ready;
+    logic [7:0] i_data_in;
     logic [15:0] baud_divisor;
     integer i;
     logic o_overrun_error;
@@ -21,6 +16,15 @@ module UART_tb;
     logic o_parity_error;
     logic [1:0] i_parity_type;
     logic uart_line;
+    logic i_full ;
+    logic i_almostfull ;
+    logic i_overflow ;
+    logic i_almostempty ;
+    logic i_underflow ;
+    logic i_wr_ack; 
+    logic i_wr_en;
+    logic o_almostfull , o_overflow , o_empty , o_almostempty , o_underflow , o_wr_ack , o_fifo_rd_en;
+    logic [7:0] o_fifo_data_out;
     UART_top top(.o_tx(uart_line) ,.i_rx(uart_line), .*);
 
     // Clock generation
@@ -35,10 +39,27 @@ module UART_tb;
 
     // Test for a Framing Error
 initial begin
-   
+  rst_n=0;
+  baud_divisor=16'd434;
+  i_parity_type=2'b01;
+  top.t1.o_fifo_rd_en=0;
+  o_fifo_rd_en=0;
+
+  @(negedge clk);
+  @(negedge clk);
+  rst_n=1;
+ for (i = 0;i<14 ;i++ ) begin
+    i_data_in=$random;
+    i_wr_en=1;
+    @(negedge clk);
+    i_wr_en=0;
+ end   
+
+wait(o_almostempty);
+o_fifo_rd_en=1;
 
 
-    rst_n = 0;
+  /*  rst_n = 0;
     baud_divisor =16'd434; 
     i_parity_type = 2'b01;
     tx_valid = 0;
@@ -58,7 +79,7 @@ initial begin
         //force uart_line = 1'b0 ;
         wait(rx_valid);
         //release uart_line ;
-
+*/
 end
     
     /*
