fdfc6e70c60674c565039b89758675bfd619db4a
YoussefAyman837 <yousefayman837@gmail.com>
2025-09-16 03:14:48 +0300

    Update UART_tb.sv

--- UART_tb.sv
+++ UART_tb.sv
@@ -1,3 +1,6 @@
+import UART_sb_pkg::*;
+import UART_trx_pkg::*;
+import UART_BFM_pkg::*;
 module UART_tb;
 
     logic clk;
@@ -25,7 +28,12 @@ module UART_tb;
     logic i_wr_en;
     logic o_almostfull , o_overflow , o_empty , o_almostempty , o_underflow , o_wr_ack , o_fifo_rd_en;
     logic [7:0] o_fifo_data_out;
-    UART_top top(.o_tx(uart_line) ,.i_rx(uart_line), .*);
+
+    UART_transaction trans = new();
+    UART_sb sb =new();
+    UART_if u_if();
+    UART_bfm bfm=new(u_if);
+    UART_top top(.* , .i_rx(u_if.rx) , .o_tx(u_if.tx));
 
     // Clock generation
     initial begin
@@ -34,98 +42,143 @@ module UART_tb;
     end
 
 
-
+assign u_if.clk = clk;
+assign u_if.rst_n = rst_n;
 
 
     // Test for a Framing Error
 initial begin
+
+  full_system_test();
+
+//loopback_test();
+//framing_error_test();
+//overrun_test();
+end
+
+task full_system_test();
+  
+  rst_n = 0;
+  top.t1.o_fifo_rd_en=0;
+  o_fifo_rd_en=0;
+  baud_divisor = 16'd434;
+  @(negedge clk);
+  @(negedge clk);
+  bfm.set_baud_rate(50000000,115200);
+  rst_n = 1;
+
+  fork
+    begin
+      sb.run();
+    end
+
+    begin
+    forever begin
+      @(negedge u_if.clk);
+      if(!o_empty)begin
+        o_fifo_rd_en=1'b1;
+        @(negedge u_if.clk);
+        sb.rx_mailbox.put(o_fifo_data_out);
+        o_fifo_rd_en=1'b0;
+      end
+    end  
+    end
+  join_none
+
+  bfm.run_serial_burst_test(10 , sb);
+  #20000;
+endtask //
+
+/*
+task overrun_test();
+  rst_n = 0;
+  top.t1.o_fifo_rd_en=0;
+  o_fifo_rd_en=0;
+  i_parity_type = 2'b00;
+  baud_divisor = 16'd434;
+  @(negedge clk);
+  @(negedge clk);
+  bfm.set_baud_rate(50000000,115200);
+  rst_n = 1;
+
+  bfm.send_burst(8);
+  repeat(10) #(8680);
+
+  bfm.send_burst(1);
+  wait(o_overrun_error);
+  $display("SUCCESS: o_overrun_error flag was asserted!");
+endtask //
+
+
+task framing_error_test();
+    $display("--- Starting Framing Error Test ---");
+  rst_n = 0;
+  top.t1.o_fifo_rd_en=0;
+  o_fifo_rd_en=0;
+  i_parity_type = 2'b01;
+  baud_divisor = 16'd434;
+  @(negedge clk);
+  @(negedge clk);
+  rst_n = 1;
+
+  bfm.set_baud_rate(50000000,115200);
+  bfm.send_corrupted_byte(8'hAA);
+  @(negedge clk);
+  wait(o_framing_error);
+  $display("SUCCESS: o_framing_error flag was asserted!");
+
+endtask //
+
+
+
+
+task loopback_test();
+    
   rst_n=0;
   baud_divisor=16'd434;
-  i_parity_type=2'b01;
   top.t1.o_fifo_rd_en=0;
   o_fifo_rd_en=0;
 
   @(negedge clk);
   @(negedge clk);
   rst_n=1;
- for (i = 0;i<14 ;i++ ) begin
-    i_data_in=$random;
+
+  fork
+    sb.run();
+  join_none
+
+ for (i = 0;i<7 ;i++ ) begin
+    wait(!i_full);
+    assert(trans.randomize());
+    i_data_in=trans.data;
+    i_parity_type=trans.parity_type;
     i_wr_en=1;
     @(negedge clk);
     i_wr_en=0;
+    sb.tx_mailbox.put(trans.data);
  end   
 
-wait(o_almostempty);
-o_fifo_rd_en=1;
+wait(i_almostempty);
 
-
-  /*  rst_n = 0;
-    baud_divisor =16'd434; 
-    i_parity_type = 2'b01;
-    tx_valid = 0;
+while(!o_empty)begin
     @(negedge clk);
-    rst_n = 1;
-
-
-
-
-        rx_ready=0;
-        wait(baud_tick_o);
-        tx_valid=1;
-        rx_ready=1;
-        tx_data=8'h01;
-        //repeat(434 * 10 ) @(posedge clk);
-
-        //force uart_line = 1'b0 ;
-        wait(rx_valid);
-        //release uart_line ;
-*/
-end
-    
-    /*
-    for (i = 0; i < 4; i = i + 1) begin
-        rx_ready=0;
-        wait(baud_tick_o);
-        tx_valid=1;
-        rx_ready=1;
-        tx_data=$random;
-        wait(rx_valid);
-    end
-    
+    o_fifo_rd_en=1;
+    @(negedge clk);
+    o_fifo_rd_en=0;
+    sb.rx_mailbox.put(o_fifo_data_out);
 end
 
+endtask //
+  */
+assert property(@(posedge clk)(top.t1.cs==3'b001 |=> !top.rx));
+assert property(@(posedge clk)((top.t1.baud_counter==baud_divisor-1) |-> top.t1.baud_tick_o));
+assert property(@(posedge clk)(top.t1.cs==3'b011 |=> top.rx==top.t1.parity ));
+assert property(@(posedge clk)(top.t1.cs==3'b100 |=> top.rx));
+assert property(@(posedge clk)(top.r1.cs==3'b011) |=> top.rx == top.r1.recieved_parity);
+assert property(@(posedge clk)(top.r1.cs==3'b011) |=> top.t1.parity == top.r1.recieved_parity);
+assert property(@(posedge clk)(top.r1.cs==3'b100 && top.r1.rx==1'b0 && top.r1.baud_tick) |=> o_framing_error);
+assert property(@(posedge clk)(top.r1.cs==3'b100 && top.r1.recieved_parity != top.r1.parity) |=> o_parity_error);
 
-*/
-  /*  initial begin
-        rst_n=0;
-        @(negedge clk);
-        rst_n=1;
-        for (i =0 ;i<4 ;i=i+1 ) begin
-            
-            tx_data=$random;
-            tx_valid=1;
-            rx_ready=1;
-            repeat(563) @(negedge clk);
-        end
-    end
-*/
 
-  /*  // Stimulus
-    initial begin
-        // Initialize
-        rst_n = 0;
-       @(negedge clk);
-       rst_n=1;
-       @(negedge clk);
-       @(negedge clk);
-       @(negedge clk);
-       rx_ready=1;
-       tx_valid=1;
-       @(negedge clk);
-       tx_data=8'b00001111;
- #500;
- rx_ready=1;
-        
-    end
-*/
 endmodule
+
